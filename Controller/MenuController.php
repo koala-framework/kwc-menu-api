3e5f9dc1d54cf5d5d32438d618daac9a75b9a268
Benjamin Hohenwarter <bh@vivid-planet.com>
2018-03-02 10:37:22 +0100

    Make root configurable

--- Controller/MenuController.php
+++ Controller/MenuController.php
@@ -4,12 +4,14 @@ namespace Kwf\KwcNativeMenuBundle\Controller;
 use Kwf\KwcNativeMenuBundle\Service;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class MenuController extends Controller
 {
     protected $_componentsService;
     protected $_tokenStorage;
+    protected $_rootComponentId;
 
     public function __construct(\Kwf\KwcNativeMenuBundle\Service\Components $componentsService, TokenStorage $tokenStorage)
     {
@@ -17,10 +19,15 @@ class MenuController extends Controller
         $this->_tokenStorage = $tokenStorage;
     }
 
+    public function setRootComponentId($rootComponentId)
+    {
+        $this->_rootComponentId = $rootComponentId;
+    }
+
     public function getComponentsAction()
     {
         $userRow = $this->_tokenStorage->getToken()->getUser()->getKwfUser();
-        return View::create(array('data'=>$this->_componentService->getDataForComponentId('root-nativeApp', $userRow)), 200);
+        return View::create(array('data'=>$this->_componentService->getDataForComponentId($this->_rootComponentId, $userRow)), 200);
     }
 
     public function getComponentAction($componentId)
